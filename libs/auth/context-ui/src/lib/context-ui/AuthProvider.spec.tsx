08745be076f6c32e7b65191068acdb605a328339
Pedro Barros <pedrobars7a@gmail.com>
2024-05-15 17:03:09 -0300

    teste wrote

--- libs/auth/context-ui/src/lib/context-ui/AuthProvider.spec.tsx
+++ libs/auth/context-ui/src/lib/context-ui/AuthProvider.spec.tsx
@@ -1,10 +1,6 @@
-// AuthProvider.test.tsx
-
 import { act, render } from '@testing-library/react';
 import React from 'react';
-import { AuthProvider, useAuth } from './AuthProvider'; // ajuste o caminho conforme necessário
-
-// Mock do localStorage
+import { AuthProvider, useAuth } from './AuthProvider';
 const localStorageMock = (() => {
   let store: { [key: string]: string } = {};
   return {
@@ -25,57 +21,55 @@ Object.defineProperty(window, 'localStorage', {
 });
 
 describe('AuthProvider', () => {
-  it('allows a user to sign in and sign out', () => {
-    let auth: ReturnType<typeof useAuth>;
+  it('allows a user to sign in and sign out', async () => {
     const TestComponent: React.FC = () => {
-      auth = useAuth();
+      const auth = useAuth();
+
+      React.useEffect(() => {
+        act(() => {
+          auth.signin({
+            token: '12345',
+            user: {
+              id: 1,
+              userName: 'johndoe',
+              name: 'John Doe',
+              profileImageUrl: 'url',
+            },
+          });
+        });
+
+        setTimeout(() => {
+          expect(auth.token).toBe('12345');
+          expect(auth.user).toEqual({
+            id: 1,
+            userName: 'johndoe',
+            name: 'John Doe',
+            profileImageUrl: 'url',
+          });
+
+          act(() => {
+            auth.signout();
+          });
+
+          expect(auth.token).toBeNull();
+          expect(auth.user).toBeNull();
+        }, 0);
+      }, []);
+
       return null;
     };
 
-    // Renderiza o componente com o AuthProvider
     render(
       <AuthProvider>
         <TestComponent />
       </AuthProvider>,
     );
-
-    // Simula a função signin
-    act(() => {
-      auth.signin({
-        token: '12345',
-        user: {
-          id: 1,
-          userName: 'johndoe',
-          name: 'John Doe',
-          profileImageUrl: 'url',
-        },
-      });
-    });
-
-    // Verifica se o signin atualizou o estado corretamente
-    expect(auth.token).toBe('12345');
-    expect(auth.user).toEqual({
-      id: 1,
-      userName: 'johndoe',
-      name: 'John Doe',
-      profileImageUrl: 'url',
-    });
-
-    // Simula a função signout
-    act(() => {
-      auth.signout();
-    });
-
-    // Verifica se o signout limpou o estado
-    expect(auth.token).toBeNull();
-    expect(auth.user).toBeNull();
   });
 
   it('persists authentication state to localStorage', () => {
     const TestComponent: React.FC = () => {
       const auth = useAuth();
 
-      // Use useEffect para garantir que as chamadas de função aconteçam após a montagem
       React.useEffect(() => {
         auth.signin({
           token: '12345',
@@ -87,7 +81,6 @@ describe('AuthProvider', () => {
           },
         });
 
-        // Verifica localStorage na próxima etapa do ciclo de vida
         setTimeout(() => {
           expect(window.localStorage.getItem('bliss-auth')).toEqual(
             JSON.stringify({
