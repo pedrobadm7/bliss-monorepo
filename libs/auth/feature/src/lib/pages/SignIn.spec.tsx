090301709b11076c5aef3d0ea3724d6d066898ac
Pedro Barros <pedrobars7a@gmail.com>
2024-05-15 19:04:12 -0300

    SignIn and QuackList

---
+++ libs/auth/feature/src/lib/pages/SignIn.spec.tsx
@@ -0,0 +1,41 @@
+import '@testing-library/jest-dom';
+
+import { MockedProvider } from '@apollo/client/testing';
+import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import { SignInPage } from './SignInPage';
+
+jest.mock('@bliss/auth/contex-ui', () => ({
+  useAuth: () => ({
+    signin: jest.fn(),
+  }),
+}));
+
+jest.mock('@bliss/auth/ui', () => ({
+  SignInTemplate: ({ isLoading, error, onSubmit }) => (
+    <form
+      onSubmit={() =>
+        onSubmit({ email: 'test@test.com', password: 'password' })
+      }
+    >
+      {isLoading ? 'Loading...' : 'Not Loading'}
+      {error && <span>Error occurred</span>}
+      <button type="submit">Sign In</button>
+    </form>
+  ),
+}));
+
+describe('SignInPage', () => {
+  it('renders without crashing', () => {
+    const { getByText } = render(
+      <MockedProvider>
+        <MemoryRouter>
+          <SignInPage />
+        </MemoryRouter>
+      </MockedProvider>,
+    );
+
+    // Verifica se o botão de Sign In está presente para indicar que o componente foi renderizado
+    expect(getByText('Sign In')).toBeInTheDocument();
+  });
+});
---
+++ libs/quack/ui/src/lib/organisms/QuackList.spec.tsx
@@ -0,0 +1,34 @@
+import '@testing-library/jest-dom';
+
+import { render } from '@testing-library/react';
+import { QuackList } from './QuackList';
+
+jest.mock('@bliss/ui', () => ({
+  Button: ({ children, onClick }) => (
+    <button onClick={onClick}>{children}</button>
+  ),
+  ErrorBanner: ({ children, title }) => (
+    <div>
+      {title}
+      {children}
+    </div>
+  ),
+  Loading: () => <div>Loading...</div>,
+}));
+
+describe('QuackList component', () => {
+  it('renders Reload button when there is an error', () => {
+    const mockError = new Error('Error loading quacks');
+
+    const { getByText } = render(
+      <QuackList
+        quacks={[]}
+        isLoading={false}
+        error={mockError}
+        refetch={() => {}}
+      />,
+    );
+
+    expect(getByText('Reload')).toBeInTheDocument();
+  });
+});
